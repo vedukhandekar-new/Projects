e47cbb1bdd074b352d16937abc29e516b414be5f
Khandekar Ved <vedu.khandekar@gmail.com>
2026-02-04 12:17:01 +0530

    model creation

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student,Product,StudentAttendance
+# Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Product)
+admin.site.register(StudentAttendance)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    studentName= models.CharField(max_length=100)
+    studentAge= models.ImageField()
+    studentCity= models.CharField(max_length=40)
+    studentEmail = models.EmailField(null=True)
+
+
+
+    class Meta:
+        db_table = "student"
+
+class Product(models.Model):
+    productName= models.CharField(max_length=100)
+    productprice= models.IntegerField()
+    productDescription= models.TextField()
+    productStock = models.IntegerField(null=True, blank=True)
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+
+
+
+
+
+    class Meta:
+        db_table = "product"
+
+
+
+class StudentAttendance(models.Model):
+    studentName = models.CharField(max_length=100)
+    date = models.DateField()
+    status = models.CharField(max_length=10)  # Present / Absent
+
+    class Meta:
+        db_table = "studentAttendance"
